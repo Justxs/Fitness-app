c797986f1ef67f1dcdbb6dd454f8216c14e13057
Karolis Žukauskas <karolis.zukaus@gmail.com>
2022-03-17 12:43:54 +0200

    Finished creating database structure

---
+++ Database/Models/NotPrefer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FitnessApp.Database.Models
+{
+    public partial class NotPrefer
+    {
+        public int FkFoodCategoryidFoodCategory { get; set; }
+        public int FkUserdataidUserdata { get; set; }
+
+        public virtual FoodCategory FkFoodCategoryidFoodCategoryNavigation { get; set; } = null!;
+    }
+}
---
+++ Database/Models/StepsRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FitnessApp.Database.Models
+{
+    public partial class StepsRecord
+    {
+        public DateTime? Date { get; set; }
+        public int? Steps { get; set; }
+        public int IdStepsRecord { get; set; }
+        public int FkCaloriesRecordidCaloriesRecord { get; set; }
+
+        public virtual CaloriesRecord FkCaloriesRecordidCaloriesRecordNavigation { get; set; } = null!;
+    }
+}
