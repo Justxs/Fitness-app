4a8df04dfa8ae7340acd9920b9c6f06037533b16
Karolis Žukauskas <karolis.zukaus@gmail.com>
2022-03-19 21:06:13 +0200

    Added a few empty pages to showcase routing

---
+++ ClientApp/src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+function LoginPage() {
+  return (
+    <div>
+      <h1>This is login page!</h1>
+    </div>
+  );
+}
+
+export default LoginPage;
---
+++ ClientApp/src/pages/PageNotFoundPage.jsx
@@ -0,0 +1,9 @@
+function PageNotFoundPage() {
+  return (
+    <div>
+      <h1>Error 404: Page Not Found</h1>
+    </div>
+  );
+}
+
+export default PageNotFoundPage;
