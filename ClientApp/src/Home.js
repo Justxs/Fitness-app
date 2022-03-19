e7b39fa39bba64b8148d56eab7861742c7e59bda
Karolis Žukauskas <karolis.zukaus@gmail.com>
2022-03-19 19:54:51 +0200

    Added bootstrap import to App.js and Home.js. Also changed Home.js to a function component

--- ClientApp/src/Home.js
+++ ClientApp/src/Home.js
@@ -1,12 +1,13 @@
 import React,{Component} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export class Home extends Component{
 
-    render(){
-        return(
-            <div className ="text-primary">
-                <h1>This is homepage</h1>
-            </div>
-        )
-    }
+function Home() {
+    return ( 
+        <div className="text-primary">
+            <h1>This is homepage</h1>
+        </div>
+     );
 }
+
+export default Home;
