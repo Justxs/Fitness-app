74f152b29d048138993c8d52c51b42ab0b4f4218
Karolis Žukauskas <karolis.zukaus@gmail.com>
2022-04-05 13:00:00 +0300

    Added login form to login page

--- ClientApp/src/pages/LoginPage.jsx
+++ ClientApp/src/pages/LoginPage.jsx
@@ -1,8 +1,16 @@
+import { useNavigate } from "react-router-dom";
+import LoginForm from "../components/login/LoginForm";
+import { useAuth } from "../hooks/useAuth";
+
 function LoginPage() {
+  const navigate = useNavigate();
+  const { signIn } = useAuth();
   return (
-    <div>
-      <h1>This is login page!</h1>
-    </div>
+    <LoginForm
+      redirection={() => navigate("/dashboard")}
+      loginFunc={signIn}
+      isLoading={false}
+    />
   );
 }
 
