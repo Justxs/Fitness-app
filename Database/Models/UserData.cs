50393646040f338e73727d0cf955ccd357a8b91c
Benas Dorochinas <benas.dorochinas@ktu.edu>
2022-04-19 21:21:21 +0300

    UserData: replace from userdatum

---
+++ Database/Models/UserData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FitnessApp.Database.Models
+{
+    public partial class UserData
+    {
+        public UserData()
+        {
+            CaloriesRecords = new HashSet<CaloriesRecord>();
+            PhysicalStateRecords = new HashSet<PhysicalStateRecord>();
+            WeightLossGoals = new HashSet<WeightLossGoal>();
+        }
+
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int ID { get; set; }
+
+        public virtual ICollection<CaloriesRecord> CaloriesRecords { get; set; }
+        public virtual ICollection<PhysicalStateRecord> PhysicalStateRecords { get; set; }
+        public virtual ICollection<WeightLossGoal> WeightLossGoals { get; set; }
+    }
+}
