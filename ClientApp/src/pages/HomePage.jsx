fc608ef9f0dfab5772ed3d88e30a57b7e0d6399e
Justas Pranauskis <justas.pranauskis@ktu.edu>
2022-04-23 22:45:33 +0300

    Minor changes
    removed Readme.md and change img tag to react-bootstrap Image tag

--- ClientApp/src/pages/HomePage.jsx
+++ ClientApp/src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import fitnessImg from '../icons/fitnessPhoto.png';
 import '../custom.scss';
-import { Button } from 'react-bootstrap'
+import { Button, Image } from 'react-bootstrap'
 
 function HomePage() {
   return (
     <div className="d-flex justify-content-center">
-      <div className="d-flex flex-column justify-content-center bg-light mt-5 p-5">
+      <div className="d-flex flex-column justify-content-center bg-light m-5 p-5">
         <h1 className='text-primary'>Welcome to SportyPal!</h1>
         <p>A pal that you need to achieve your goals. With this website you will be able
           to see your progress and change your routine wich will help you get your wanted 
@@ -16,7 +16,9 @@ function HomePage() {
             <b className='text-light'>Get started</b>
           </Button>
         </div>
-        <img className='img-fluid' src={fitnessImg}  alt="Fitness" />
+
+        <Image fluid src={fitnessImg} alt="Fitness" />
+
       </div>
     </div>
   );
