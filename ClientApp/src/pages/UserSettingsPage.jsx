e80fd12f858590bf552b11037b9dbaed4b28e15f
Pijus-N <75778499+Pijus-N@users.noreply.github.com>
2022-04-25 16:22:39 +0300

    Settings page update without collapsable items

--- ClientApp/src/pages/UserSettingsPage.jsx
+++ ClientApp/src/pages/UserSettingsPage.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { Button } from 'react-bootstrap';
 import useCollapse from 'react-collapsed';
 import '../settings.css';
+import { Form, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 
 
 function UserSettingsPage() {
@@ -138,137 +139,11 @@ function handleOnClick() {
 
     
 
-    <div className="preferences">
-             <Section title="General" defaultExpanded="true">
-                <label>
-                    <input type="checkbox"/> Use dark theme
-                </label>
-                
-                <br/><br/>
-            </Section>
-             <Section title="Account">
-             
-            <ul className="list-group">
-              <li className="list-group-item mb-2">
-                <h6 className="font-weight-bold">Change Email Address</h6>
-                <p>Update your email address</p>
-                <form>
-                <label className="mx-2">New email:
-                <input type="text" />
-                </label>
-                <Button className="ml-1">Change</Button>
-                </form>
-
-              </li>
-
-              <li className="list-group-item mb-2">
-                <div className="font-weight-bold">
-                <h6 className="font-weight-bold">Change password</h6>
-                <p>Change your current password</p>
-                <form>
-                <label className="mx-2">New password: 
-                <input type="password" className="mx-5" />
-                </label>
-                <label className="mx-2">Confirm password: 
-                <input type="password" className="mx-4" />
-                </label>
-                <Button className="ml-1">Change</Button>
-                </form>
-                </div>
-
-              </li>
+   
 
 
-
-            </ul>
-
-                        
-               
-            
-            </Section>
-            <Section title="Body Data">
-            <ul className="list-group">
-              <li className="list-group-item mb-2">
-                <h6 className="font-weight-bold">Weight</h6>
-                <p>Update or set your weight</p>
-                <form>
-                <label className="mx-2">Weight:
-                <input type="text" />
-                </label>
-                <Button className="ml-1">Set</Button>
-                </form>
-
-              </li>
-
-              <li className="list-group-item mb-2">
-                <h6 className="font-weight-bold">Height</h6>
-                <p>Update or set your height</p>
-                <form>
-                <label className="mx-2">Height:
-                <input type="text" />
-                </label>
-                <Button className="ml-1">Set</Button>
-                </form>
-
-              </li>
-
-
-
-            </ul>
-
-                        
-               
-            </Section>
-
-            <Section title="Goals">
-            <ul className="list-group">
-              <li className="list-group-item mb-2">
-                <h6 className="font-weight-bold">Weight Goal</h6>
-                <p>Update or set your weight goal</p>
-                <form>
-                <label className="mx-2">Weight Goal:
-                <input type="text" />
-                </label>
-                <Button className="ml-1">Set</Button>
-                </form>
-
-              </li>
-
-              <li className="list-group-item mb-2">
-                <h6 className="font-weight-bold">Calorie Intake Goal</h6>
-                <p>Update or set your calorie intake goal</p>
-                <form>
-                <label className="mx-2">Daily calories:
-                <input type="text" />
-                </label>
-                <Button className="ml-1">Set</Button>
-                </form>
-
-              </li>
-
-
-
-            </ul>
-
-                        
-               
-            </Section>
-
-
-
-
-
-
-
-            <Section title="Notifications" collapsedHeight="32">
-                
-                <br/><br/>
-                <label>
-                    <input type="checkbox"/> Notify me about my progress via Email
-                </label>
-                <br/><br/>
-            </Section>
-       </div>
+       
+     
 
     
 
@@ -315,6 +190,188 @@ function handleOnClick() {
 
 
     </div>
+   
+
+      <div class="mx-3 div-2"> 
+        <h2 className="mx=2">General </h2>
+        <div className="mb=5 background-custom"  >
+        <label className="mx-2 "> 
+                    <input type="checkbox"/> Use dark theme
+                </label>
+                <br></br><br></br>
+                    
+          
+
+
+
+        </div>
+
+
+
+        <h2 className="mx=2">Account</h2>
+
+          <div className=" background-custom">
+            <div className="mx-2">
+          <h6 className="mb=2">Change Email Address</h6>
+                          <Form>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>New Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    
+                  </Form.Group>
+
+                 
+                  <Button variant="primary" type="submit">
+                    Change
+                  </Button>
+                  <br></br><br></br>
+
+                <h6 className="mb=2">Change Password</h6>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                  </Form.Group>
+                  
+
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                    
+                  </Form.Group>
+
+                  <Button variant="primary" type="submit">
+                    Change
+                  </Button>
+                  <br></br><br></br>
+
+                </Form>
+
+          </div>
+          </div>
+
+          <h2 className="mx=2">Body data</h2>
+          <div className=" background-custom">
+            <div className="mx-2">
+          <h6 className="mb=2">Update your weight</h6>
+                          <Form>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    
+                    <Form.Control type="text" placeholder="Weight" />
+                    
+                  </Form.Group>
+
+                 
+                  <Button variant="primary" type="submit">
+                    Set
+                  </Button>
+                  <br></br><br></br>
+
+                <h6 className="mb=2">Update Your Height</h6>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                   
+                    <Form.Control type="text" placeholder="Height" />
+                  </Form.Group>
+                  
+
+
+                  <Button variant="primary" type="submit">
+                    Set
+                  </Button>
+                  <br></br><br></br>
+
+                </Form>
+
+          </div>
+          </div>
+
+
+
+          <h2 className="mx=2">Goals</h2>
+          <div className=" background-custom">
+          <div className="mx-2">
+          <h6 className="mb=2">Update your weight goals</h6>
+                          <Form>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    
+                    <Form.Control type="text" placeholder="Weight goal" />
+                    
+                  </Form.Group>
+
+                 
+                  <Button variant="primary" type="submit">
+                    Set
+                  </Button>
+                  <br></br><br></br>
+
+                <h6 className="mb=2">Update Your Daily Calories Intake Goal</h6>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                   
+                    <Form.Control type="text" placeholder="Calorie Intake Goal" />
+                  </Form.Group>
+                  
+
+
+                  <Button variant="primary" type="submit">
+                    Set
+                  </Button>
+                  <br></br><br></br>
+
+                </Form>
+
+          </div>
+          </div>
+
+
+          <h2 className="mx=2">Notifications </h2>
+        <div className=" background-custom">
+        <div className="mx-2">
+        <label>
+                    <input type="checkbox"/> Receive updates via Email
+                </label>
+                    
+          
+                <br></br><br></br>
+
+
+        </div>
+        </div>
+          
+
+
+
+
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   );
 }
