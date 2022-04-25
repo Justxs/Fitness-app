0d83b54da20ef67e25866f32349714b12e8a99ca
Karolis Žukauskas <karolis.zukaus@gmail.com>
2022-04-26 01:46:41 +0300

    Added Automapper for mapping between DTOs and database classes. Added swagger for backend testing purposes. Added FitnessAppDbContext to Program.cs

---
+++ Configuration/AutomapperConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FitnessApp.Database.DTO;
+using FitnessApp.Database.Models;
+
+namespace FitnessApp.Configuration
+{
+    public class AutoMapperConfiguration: Profile
+    {
+        public AutoMapperConfiguration()
+        {
+            CreateMap<FoodProductDtoCreate, FoodProduct>(MemberList.None);
+            CreateMap<FoodProductDtoUpdate, FoodProduct>(MemberList.None);
+            CreateMap<FoodProduct, FoodProductDtoGet>(MemberList.None);
+            CreateMap<FoodProductDtoGet, FoodProduct>(MemberList.None);
+        }
+    }
+}
