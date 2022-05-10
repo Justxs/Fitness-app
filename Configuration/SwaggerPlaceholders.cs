8b2baff1315d980ce208f081dd8c58d3f7ca9ab2
Karolis Žukauskas <karolis.zukaus@gmail.com>
2022-05-10 17:43:59 +0300

    Updated AutoMapper config Updated SwaggerPlaceholders

--- Configuration/SwaggerPlaceholders.cs
+++ Configuration/SwaggerPlaceholders.cs
@@ -1,5 +1,4 @@
 ﻿using FitnessApp.Database.DTO;
-using FitnessApp.Database.DTO.EatingActivityRecord;
 using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
@@ -20,14 +19,6 @@ namespace FitnessApp.Configuration
                     ["fats100g"] = new OpenApiFloat(3.3f),
                     ["ImageUrl"] = new OpenApiString("https://www.thespruceeats.com/thmb/ZJyWw36nZ1lLNi5FHOKRy9daQqs=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/loaf-of-bread-182835505-58a7008c5f9b58a3c91c9a14.jpg"),
 
-                };
-            }
-            if(context.Type == typeof(EatingActivityRecordDtoForm))
-            {
-                schema.Example = new OpenApiObject()
-                {
-                    ["grams"] = new OpenApiInteger(123),
-                    ["foodProductId"] = new OpenApiInteger(1)
                 };
             }
         }
